3de12ac4f063afd9010f2ec202152219e090253f
LineNumber0 <linenumber0@gamil.com>
2018-12-30 04:22:10 +0100

    LineNumber0 commit message from the Terminal with sync.sh file

---
+++ JS/followLyrics.js
@@ -0,0 +1,21 @@
+﻿// {LN_0}
+var aplayer = document.getElementById("audplayer"), 
+LyricTimes = [],
+LyricTarget = document.querySelectorAll("a[id^='lp']:empty"),
+LyricElement = document.querySelector("a[id^='lp']:empty+pre");
+aplayer.volume = 0.5;
+function getStime() {
+  return parseInt(aplayer.currentTime);
+}
+for (var i = 0; i < LyricTarget.length; i++) {
+  LyricTarget[i].style.right = parseInt(getComputedStyle(LyricElement).getPropertyValue("width")) / 2 + "px";
+}
+function followLyrics() {
+  for (var j = 0; j < LyricTimes.length; j++) {
+    switch(getStime()) {
+    case LyricTimes[j]: document.location.href = "#lp" + j;
+    break;
+  }
+  }
+}
+aplayer.addEventListener("timeupdate",followLyrics);
